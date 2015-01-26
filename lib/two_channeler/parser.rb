ce3d027525103fb0e7971eae3c1584467825ce61
cnosuke <shinnosuke@gmail.com>
2015-01-26 14:51:25 +0900

    fix indent

--- lib/two_channeler/parser.rb
+++ lib/two_channeler/parser.rb
@@ -2,14 +2,14 @@ require 'open-uri'
 
 module TwoChanneler
   class Parser
-  	def self.parse(file)
+    def self.parse(file)
       doc = read_dat(file)
       doc.split("\n").map{|e| e.split("<>")}
     end
 
     def self.read_dat(file)
       open(file, 'r:Shift_JIS').read
-      .encode("UTF-8", :invalid => :replace, :undef => :replace)  
+      .encode("UTF-8", :invalid => :replace, :undef => :replace)
     end
   end
 end
--- lib/two_channeler/thread.rb
+++ lib/two_channeler/thread.rb
@@ -1,38 +1,39 @@
 require './two_channeler'
 
 module TwoChanneler
-	class Thread
-		attr_reader :subject, :responses, :dat_path, :board_path
-		def initialize(dat_path: nil, subject: nil, count: nil, board_path: nil, fetch: false)
-			@dat_path = dat_path
-			@subject = subject
-			@count = count
-			@board_path = "#{board_path}dat/" if board_path
-			@responses = []
-			fetch_thread if fetch
-		end
-		def fetch_thread
-			begin
-				d = TwoChanneler::Parser.parse(@board_path.to_s + @dat_path)
-				@subject = d.first.fetch(-1)
-				@responses = d.map.with_index do |res, idx|
-					Response.new(res, self, idx)
-				end
-				@responses.map do |r|
-					r.anchor = @responses.select{|i| r.anchor_to.include? i.index }
-					r.anchor_to.each do |anchor|
-						@responses[anchor - 1].anchored << r if @responses[anchor - 1]
-					end
-				end
-				@fetch = true
-				self
-			rescue => e
-				p e
-			end
-		end
-		def inspect
-			return <<EOB
-#<TwoChanneler::Thread
+  class Thread
+    attr_reader :subject, :responses, :dat_path, :board_path
+    def initialize(dat_path: nil, subject: nil, count: nil, board_path: nil, fetch: false)
+      @dat_path = dat_path
+      @subject = subject
+      @count = count
+      @board_path = "#{board_path}dat/" if board_path
+      @responses = []
+      fetch_thread if fetch
+    end
+    def fetch_thread
+      begin
+        d = TwoChanneler::Parser.parse(@board_path.to_s + @dat_path)
+        @subject = d.first.fetch(-1)
+        @responses = d.map.with_index do |res, idx|
+         Response.new(res, self, idx)
+        end
+        @responses.map do |r|
+          r.anchor = @responses.select{|i| r.anchor_to.include? i.index }
+          r.anchor_to.each do |anchor|
+            @responses[anchor - 1].anchored << r if @responses[anchor - 1]
+          end
+        end
+        @fetch = true
+        self
+      rescue => e
+        p e
+      end
+    end
+
+    def inspect
+      return <<EOB
+  #<TwoChanneler::Thread
   @subject=#{@subject.inspect},
   @responses.size=#{@responses.size},
   @count=#{@count.inspect},
@@ -40,6 +41,6 @@ module TwoChanneler
   @board_path=#{@board_path.inspect}
  >
 EOB
-		end
-	end
+    end
+  end
 end
